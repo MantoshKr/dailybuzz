54b7c6d0e677b82ef3584cbbcf77ead1d979bd88
mantoshdev <123961664+mantoshdev2@users.noreply.github.com>
2023-05-29 23:49:43 +0530

    fetching and displaying NewsApi data

---
+++ src/components/NewsList.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { NewsItem } from '.'
+
+const NewsList = () => {
+    const [articles, setArticles] = useState([])
+
+    useEffect(() => {
+        const getArticles = async () => {
+            const response = await axios.get(`https://newsapi.org/v2/everything?q=tesla&from=2023-09-18&sortBy=publishedAt&apiKey=NEWSAPIKEY`)
+            setArticles(response.data.articles)
+            console.log(response)
+        }
+
+        getArticles()
+    }, [])
+    return (
+        <div>
+            {articles.map(article => {
+                return(
+                    <NewsItem 
+                        title={article.title}
+                        description={article.description}
+                        url={article.url}
+                        urlToImage={article.urlToImage} 
+                    />
+                )
+            })}
+        </div>
+    )
+}
+
+export default NewsList
